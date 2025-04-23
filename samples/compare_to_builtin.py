73b02439c6b54bfd785d269511fbe62f591927a4
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-04-23 18:08:47 +0300

    Sort output alphabetically

--- samples/compare_to_builtin.py
+++ samples/compare_to_builtin.py
@@ -25,18 +25,17 @@ def main():
             continue
 
     print(f'There are {len(differ):,} types where mimetypes and content-types disagree')
-    for (mk, mv), (ct_k, ct_v) in differ:
+    for (mk, mv), (ct_k, ct_v) in sorted(differ):
         print(f'mimetypes: {mk} {mv}, content-types: {ct_k} {ct_v}')
     print()
 
     print(f'There are {len(in_mime_only):,} types in mimetypes that are not in content-types')
-    for k, v in in_mime_only:
+    for k, v in sorted(in_mime_only):
         print(f'{k.ljust(5)}: {v}')
     print()
 
     print(f'There are {len(only_ct):,} types in content-types that are not in mimetypes')
-    print('in_ct_only')
-    for k, v in only_ct:
+    for k, v in sorted(only_ct):
         print(f'.{k.ljust(5)} -> {v}')
     print()
 
